e064a9401691b3c3dd739fa1e6259ae14e6f877a
szt02 <szt02@szlgbp.hu>
2021-05-28 08:18:56 +0200

    message

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+/*app.get('/', function(req,res){
+    res.end('Hello World!');
+});*/
+
+app.use(express.static('public'));
+
+app.listen(9000);
